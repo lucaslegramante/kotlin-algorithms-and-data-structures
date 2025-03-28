92a4184a2f993cde398ca84f314566e6fb546191
lucaslegramante <lucaslegramante@gmail.com>
2025-03-27 22:32:03 -0300

    feat: create more functions

---
+++ src/main/kotlin/semana1Fundamentos/MainSemana1.kt
@@ -0,0 +1,61 @@
+package org.example.semana1Fundamentos
+
+fun calcularSoma(lista: List<Int>): Int = lista.sum()
+
+fun calcularMedia(lista: List<Int>): Double = lista.average()
+
+fun encontrarMaior(lista: List<Int>): Int? = lista.maxOrNull()
+
+fun encontrarMenor(lista: List<Int>): Int? = lista.minOrNull()
+
+fun filtrarPares(lista: List<Int>): List<Int> = lista.filter { it % 2 == 0 }
+
+fun retornaPrimo(numero: Int): List<Int> {
+    val listaPrimos = mutableListOf<Int>()
+    for (i in 2..numero) {
+        if (i == 2) {
+            listaPrimos.add(i)
+        } else {
+            var isPrimo = true
+            for (j in 2 until i) {
+                if (i % j == 0) {
+                    isPrimo = false
+                    break
+                }
+            }
+            if (isPrimo) {
+                listaPrimos.add(i)
+            }
+        }
+    }
+
+    return listaPrimos
+}
+
+fun ordenarLista(lista: List<Int>): List<Int> = lista.sorted()
+
+fun ordenarListaManual(lista: List<Int>): List<Int> {
+    val listaOrdenada = mutableListOf<Int>()
+    val listaCopia = lista.toMutableList()
+
+    while (listaCopia.isNotEmpty()) {
+        val menor = listaCopia.minOrNull()
+        menor?.let {
+            listaOrdenada.add(it)
+            listaCopia.remove(it)
+        }
+    }
+
+    return listaOrdenada
+}
+
+fun main() {
+    println("Calculando a soma: ${calcularSoma(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Filtrando os pares: ${filtrarPares(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Calculando a média: ${calcularMedia(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Encontrando o maior: ${encontrarMaior(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Encontrando o menor: ${encontrarMenor(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Encontrando os primos: ${retornaPrimo(13)}")
+    println("Ordenando a lista: ${ordenarLista(listOf(3, 10, 7, 2, 8, 5, 1))}")
+    println("Ordenando a lista manualmente: ${ordenarListaManual(listOf(3, 10, 7, 2, 8, 5, 1))}")
+}
--- src/test/kotlin/testesSemana1/TestesSemana1.kt
+++ src/test/kotlin/testesSemana1/TestesSemana1.kt
@@ -1,7 +1,10 @@
 package testesSemana1
 
-import org.example.calcularSoma
-import org.example.filtrarPares
+import org.example.semana1Fundamentos.calcularMedia
+import org.example.semana1Fundamentos.calcularSoma
+import org.example.semana1Fundamentos.encontrarMaior
+import org.example.semana1Fundamentos.encontrarMenor
+import org.example.semana1Fundamentos.filtrarPares
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
 
@@ -16,5 +19,23 @@ class TestesSemana1 {
     fun testFiltrarPares() {
         val numeros = listOf(3, 10, 7, 2, 8, 5, 1)
         assertEquals(listOf(10, 2, 8), filtrarPares(numeros))
+    }
+
+    @Test
+    fun testCalcularMedia() {
+        val numeros = listOf(3, 10, 7, 2, 8, 5, 1)
+        assertEquals(5.142857142857143, calcularMedia(numeros))
+    }
+
+    @Test
+    fun testEncontrarMaior() {
+        val numeros = listOf(3, 10, 7, 2, 8, 5, 1)
+        assertEquals(10, encontrarMaior(numeros))
+    }
+
+    @Test
+    fun testEncontrarMenor() {
+        val numeros = listOf(3, 10, 7, 2, 8, 5, 1)
+        assertEquals(1, encontrarMenor(numeros))
     }
 }
